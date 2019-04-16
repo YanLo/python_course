c55e1bc83a2082ea3ef837567d4200939761de0c
YanLo <logovoya@tut.by>
2019-04-16 22:34:52 +0300

    update

--- week_2/key_value_storage.py
+++ week_2/key_value_storage.py
@@ -19,10 +19,18 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--key", action='store', help="key of the value")
 parser.add_argument("--value", action='store', help="value corresponding to a key")
 
+def add_key_value(dictionary, key, value):
+    if key not in dictionary:
+        dictionary[key] = value
+    elif type(dictionary[key]) == list:
+        dictionary[key].append(value)
+    else:
+        dictionary[key] = [dictionary[key], value]
+
 args = parser.parse_args()
 if args.key and not args.value:
     print(key_value_storage.get(args.key, None))
 elif args.key and args.value:
-    key_value_storage[args.key] = args.value
+    add_key_value(key_value_storage, args.key, args.value)
     with open(storage_path, 'w') as storage_file:
         json.dump(key_value_storage, storage_file)
