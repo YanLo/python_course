26e3181f6558ae9dacf59594631e6be8771ff8b6
YanLo <logovoya@tut.by>
2019-04-16 21:04:30 +0300

    working storage

--- week_2/key_value_storage.py
+++ week_2/key_value_storage.py
@@ -1,25 +1,23 @@
 import os
+import json
 import tempfile
 import argparse
-import json
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--key", type=any , help="key of the value")
-parser.add_argument("--value", type=any, help="value corresponding to key")
-args = parser.parse_args()
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-
-with open(storage_path, 'w') as storage_file:
-    pass
-
 with open(storage_path, 'r') as storage_file:
-    key_value_storage = json.load(storage_file)
-    print(key_value_storage)
+    try:
+        key_value_storage = json.load(storage_file)
+    except ValueError:
+        key_value_storage = {}
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--key", action='store', help="key of the value")
+parser.add_argument("--value", action='store', help="value corresponding to a key")
+
+args = parser.parse_args()
 if args.key and not args.value:
-    print(key_value_storage.get(str(args.key), None))
+    print(key_value_storage.get(args.key, None))
 elif args.key and args.value:
-    key_value_storage[str(args.key)] = args.value
+    key_value_storage[args.key] = args.value
     with open(storage_path, 'w') as storage_file:
         json.dump(key_value_storage, storage_file)
