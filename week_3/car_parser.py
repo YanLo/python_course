0b89972271f818218ba419b404e3c7b3b054318c
YanLo <logovoya@tut.by>
2019-04-27 21:53:39 +0300

    car_parser done

--- week_3/car_parser.py
+++ week_3/car_parser.py
@@ -13,30 +13,25 @@ class CarBase:
 
 class Car(CarBase):
     def __init__(self, car_type, brand, photo_file_name, carrying, passenger_seats_count):
-        super().__init__(self, car_type, brand, photo_file_name, carrying)
+        super().__init__(car_type, brand, photo_file_name, carrying)
         self.passenger_seats_count = int(passenger_seats_count)
 
 class Truck(CarBase):
     def __init__(self, car_type, brand, photo_file_name, carrying, body_whl):
-        super().__init__(self, car_type, brand, photo_file_name, carrying)
-        self.body_whl = body_whl
-        val_list = self.body_whl.split("x")
-        if (val_list == ['']):
-            self.body_length = self.body_width = self.body_hight = 0
-        else:
-            try:
-                self.body_length = float(body_parameters_list[0])
-                self.body_width = float(body_parameters_list[1])
-                self.body_hight = float(body_parameters_list[2])
-            except ValueError:
-                self.body_length = self.body_width = self.body_hight = 0
+        super().__init__(car_type, brand, photo_file_name, carrying)
+        self.body_length = self.body_width = self.body_height = 0
+        val_list = body_whl.split("x")
+        if (val_list != ['']):
+            self.body_length = float(val_list[0])
+            self.body_width = float(val_list[1])
+            self.body_height = float(val_list[2])
 
     def get_body_volume(self):
-        return self.body_length * self.body_width * self.body_hight
+        return self.body_length * self.body_width * self.body_height
 
 class SpecMachine(CarBase):
     def __init__(self, car_type, brand, photo_file_name, carrying, extra):
-        super().__init__(self, car_type, brand, photo_file_name, carrying)
+        super().__init__(car_type, brand, photo_file_name, carrying)
         self.extra = extra
 
 
@@ -46,9 +41,31 @@ def get_car_list(csv_filename):
         reader = csv.reader(csv_file, delimiter=';')
         next(reader)
         for row in reader:
-            print(row)
+            try:
+                car_type = row[0]
+                brand = row[1]
+                passenger_seats_count = row[2]
+                photo_file_name = row[3]
+                body_whl = row[4]
+                carrying = row[5]
+                extra = row[6]
+                if(car_type == 'car'):
+                    new_car = Car(car_type, brand, photo_file_name, carrying, passenger_seats_count)
+                    car_list.append(new_car)
+                if(car_type == 'truck'):
+                    new_car = Truck(car_type, brand, photo_file_name, carrying, body_whl)
+                    car_list.append(new_car)
+                if(car_type == 'spec_machine'):
+                    new_car = SpecMachine(car_type, brand, photo_file_name, carrying, extra)
+                    car_list.append(new_car)
+            except Exception as exception:
+                pass
+                #print("-----------------EXCEPTION_OCCURED:   ", exception)
 
     return car_list
 
-if __name__ == "__main__":
-    print(get_car_list("cars.csv"))
+#if __name__ == "__main__":
+#    car_list = get_car_list("cars.csv")
+#    for car in car_list:
+#        print(car.car_type, car.brand)
+
