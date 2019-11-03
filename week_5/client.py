2ab3ddb467634e7d8ad31d1a0477c3324cb62300
YanLo <logovoya@tut.by>
2019-11-03 18:16:52 +0300

    in process

--- week_5/client.py
+++ week_5/client.py
@@ -1,41 +1,57 @@
 import socket
 import time
 
+
 class ClientError(Exception):
     pass
 
+
 class Client:
-    def __init__(host, port_num, timeout=None):
+    def __init__(self, host, port_num, timeout=None):
         self.host = host
         self.port_num = port_num
         self.timeout = timeout
 
-    def put(metric_name, metric_val, timestamp):
-        try:
-            with socket.create_connection(self.host, self.port_num) as sock:
-                if (timestamp == None):
-                    sock.sendall((metric_name, metric_val, str(time.time)))
+    def put(self, key, value, timestamp=str(time.time())):
+        with socket.create_connection(self.host, self.port_num) as sock:
+            message = 'put {} {} {}\n'.format(key, value, timestamp)
+            sock.sendall(message)
+            answer = sock.recv(1024)
+            str_ans = answer.decode()
+            condition = (str_ans.split('\n'))[0]
+            if(condition == 'error'):
+                raise ClientError
+
+    def get(self, key):
+        with socket.create_connection(self.host, self.port_num) as sock:
+            message = 'get {}\n'.format(key)
+            sock.sendall(message)
+
+            all_data = []
+            while True:
+                data = sock.recv(1024)
+                all_data.append(data)
+                if not data:
+                    break
+
+            buffer = all_data.decode()
+            message = buffer.split('\n')
+            condition = message[0]
+            if(condition == 'error'):
+                raise ClientError
+            info_messages = message[1:-2]
+            key_val_dict = {}
+            for item in info_messages:
+                splitted_item = item.split()
+                key = splitted_item[0]
+                value = float(splitted_item[1])
+                timestamp = int(splitted_item[2])
+                if key not in key_val_dict:
+                    key_val_dict[key] = (value, timestamp)
+                elif type(key_val_dict[key]) == list:
+                    key_val_dict[key].append((value, timestamp))
                 else:
-                    sock.sendall((metric_name, metric_val, timestamp))
-        except ClientError:
-            pass
+                    key_val_dict[key] = [key_val_dict[key], (value, timestamp)]
+                key_val_dict[key] = sorted(key_val_dict[key], key=lambda x: x[0])
 
-    def get(metric_name):
-        try:
-            with socket.create_connection(self.host, self.port_num) as sock:
-                #send metric_name for getting an answer
-                sock.sendall(metric_name)
-
-                #now we actually get an answer
-                sock.bind((self.host, self.port_num))
-                sock.listen()
-                conn, addr = sock.accept()
-                with conn:
-                    while True:
-                        data = conn.recv(1024)
-                        if not data:
-                            break
-                        print(data.decode("utf8"))
-
-        except ClientError:
-            pass
+            return key_val_dict
